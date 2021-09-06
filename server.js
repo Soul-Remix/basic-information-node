7c69854db5b6eec16c95481945c1651526aa8789
Soul-Remix <soulremix-webdev@pm.me>
2021-09-06 15:48:36 +0300

    refactor code and fix redirect to 404

--- server.js
+++ server.js
@@ -1,37 +1,50 @@
 const http = require('http');
 const fs = require('fs');
+const path = require('path');
 
 const port = process.env.PORT || 8080;
 
-const filePath = './pages/';
-
 const server = http.createServer((req, res) => {
-  let file;
-  switch (req.url) {
-    case '/':
-      file = filePath + 'index.html';
-      res.statusCode = 200;
+  let filePath = path.join(
+    __dirname,
+    'pages',
+    req.url === '/' ? 'index.html' : req.url
+  );
+
+  let extname = path.extname(filePath);
+
+  let contentType = 'text/html';
+
+  switch (extname) {
+    case '.js':
+      contentType = 'text/javascript';
       break;
-    case '/about':
-      file = filePath + 'about.html';
-      res.statusCode = 200;
+    case '.css':
+      contentType = 'text/css';
       break;
-    case '/contact':
-      file = filePath + 'contact-me.html';
-      res.statusCode = 200;
-      break;
-    default:
-      file = filePath + '404.html';
-      res.statusCode = 404;
   }
 
-  fs.readFile(file, (err, data) => {
+  if (contentType == 'text/html' && extname == '') filePath += '.html';
+
+  fs.readFile(filePath, (err, content) => {
     if (err) {
-      console.log(err);
-      res.end('err');
+      if (err.code == 'ENOENT') {
+        fs.readFile(
+          path.join(__dirname, 'pages', '404.html'),
+          (err, content) => {
+            res.writeHead(404, { 'Content-Type': 'text/html' });
+            res.end(content, 'utf8');
+          }
+        );
+      } else {
+        res.writeHead(500);
+        res.end(`Server Error: ${err.code}`);
+      }
+    } else {
+      // Success
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(content, 'utf8');
     }
-    res.setHeader('content-type', 'text/html');
-    res.end(data);
   });
 });
 
